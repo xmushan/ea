88db79f42cf07d9b0e237cb61164b8fe05de3487
xubin <xmushan@163.com>
2024-08-12 22:22:52 +0800

    update

--- rsi指标.py
+++ rsi指标.py
@@ -1,21 +1,22 @@
 import MetaTrader5 as mt5
 import pandas as pd
 import numpy as np
-from datetime import datetime, timedelta, timezone
+from datetime import datetime, timedelta, timezone,time
 from config import login, server, password
-import time
+import time as timeSleep
 
 # 初始化MT5
 mt5.initialize()
 authorized = mt5.login(login=login, password=password, server=server)
+print(authorized)
 if not authorized:
     print(f"Failed to login to MT5: {mt5.last_error()}")
     mt5.shutdown()
     exit()
 
-symbol = "BTCUSD"  # 交易符号
+symbol = "XAUUSDc"  # 交易符号
 timeframe = mt5.TIMEFRAME_M15  # 时间框架
-lot_size = 0.05  # 每次交易的手数
+lot_size = 0.02  # 每次交易的手数
 bars = 100  # 获取最近100个柱数据
 slippage = 5  # 允许的价格滑点
 last_kline_time = None  # 用于存储上一次K线时间戳
@@ -46,6 +47,21 @@ def calculate_rsi(data, period=14):
     rs = avg_gain / avg_loss
     rsi = 100 - (100 / (1 + rs))
     return rsi.iloc[-1]
+
+def calculate_cci(data,period=14):
+    data['time'] = pd.to_datetime(data['time'], unit='s')
+    # 计算典型价格 (Typical Price)
+    data['TP'] = (data['high'] + data['low'] + data['close']) / 3
+    # 计算14周期的移动平均
+    data['SMA'] = data['TP'].rolling(window=period).mean()
+    # 计算偏差
+    data['Mean Deviation'] = data['TP'].rolling(window=period).apply(lambda x: np.mean(np.abs(x - np.mean(x))))
+    # 计算CCI
+    data['CCI'] = (data['TP'] - data['SMA']) / (0.015 * data['Mean Deviation'])
+    # 获取最后一条数据的CCI值
+    last_cci = data['CCI'].iloc[-1]
+    return last_cci
+
 
 
 # 计算布林带
@@ -81,12 +97,22 @@ def open_order(symbol, lot, order_type, price):
         print(result)
 
 # 当前订单是否获得收益
-def checkCurrentIsprofit():
+def checkCurrentIsprofit(flag = True,isAll = False):
     orders = mt5.positions_get()
-    if orders is None:
-        print(f"No open orders found: {mt5.last_error()}")
-        return None
+    total = 0
+    if not orders:
+        return
     orders_df = pd.DataFrame(list(orders), columns=orders[0]._asdict().keys())
+    for index, order in orders_df.iterrows():
+        total = total + order['profit']
+    if (flag == False):
+        for index, order in orders_df.iterrows():
+            set_protective_stop(order)
+        return
+    if (isAll == True and total >= 20):
+        for index, order in orders_df.iterrows():
+            set_protective_stop(order)
+        return
     for index, order in orders_df.iterrows():
         if order['profit'] >= 5:
             set_protective_stop(order)
@@ -120,38 +146,64 @@ def get_current_kline_time(symbol, timeframe):
     return utc_time
 
 
+def is_within_business_hours():
+    # 获取当前时间
+    current_time = datetime.now().time()
+    # 定义工作时间范围
+    start_time = time(7, 30, 0)  # 早上 7.30 点
+    end_time = time(18, 0, 0)   # 晚上 18 点
+    # 判断当前时间是否在工作时间范围内
+    return start_time <= current_time <= end_time
+
 def main():
     positions_total=mt5.positions_total()
-    if positions_total >= 10:
-        checkCurrentIsprofit()
-        print('已达当前最大订单量')
-        return
+    isWorking = is_within_business_hours()
+    # if (isWorking == False):
+    #     print('时间不符合')
+    #     checkCurrentIsprofit(False)
+    #     return
+    # if positions_total >= 5:
+    #     checkCurrentIsprofit()
+    #     print('已达当前最大订单量')
+    #     return
     data = get_historical_data(symbol, timeframe)
     upper,lower = CalculateBollingerBands(data)
-    rsi = calculate_rsi(data)
+    rsi = calculate_rsi(data,20)
+    cci = calculate_cci(data,20)
     bid, ask = get_current_price(symbol)
     global last_kline_time
     current_kline_time = get_current_kline_time(symbol, timeframe)
     if (last_kline_time == current_kline_time):
         print('当前K线下过单')
         return
-    # 如果rsi指标小于30，执行买多操作
-    if (rsi < 30 and ask < lower):
+    # 如果rsi指标小于60,cci小于0，执行买多操作
+    if (rsi <= 60 and cci <= 0 and ask < lower):
         checkCurrentIsprofit()
         open_order(symbol, lot_size, mt5.ORDER_TYPE_BUY, ask)
         last_kline_time = current_kline_time
-    # rsi指标大于70，执行做空操作
-    elif (rsi > 76 and bid > upper):
+    # 如果rsi指标小于25，执行买多操作
+    if ((rsi <= 25 and ask < lower) or cci < -100):
+        checkCurrentIsprofit()
+        open_order(symbol, lot_size, mt5.ORDER_TYPE_BUY, ask)
+        last_kline_time = current_kline_time
+    # rsi指标大于75，执行做空操作    
+    elif (rsi >= 80 and bid > upper):
         checkCurrentIsprofit()
         open_order(symbol, lot_size, mt5.ORDER_TYPE_SELL, bid)
         last_kline_time = current_kline_time
+    # rsi指标在40 和65之间，检查收益
+    elif (cci >= 150):
+        checkCurrentIsprofit()
+        print('rsi指标在40 和65之间，检查收益')
     else:
         print('无信号')
         print(rsi)
         print(upper,lower) 
         print(bid,ask)
+        checkCurrentIsprofit(True,True)
+
 
 while True:
     main()
-    time.sleep(2)
+    timeSleep.sleep(1)
 
